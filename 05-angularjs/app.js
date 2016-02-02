71013530047e7fe2531c103cc30da3af2aee9ea7
Agustín Gómez <gomez.orc@gmail.com>
2016-02-02 03:30:08 -0300

    A few updates (still not fully working)
    - Tracks only have a title for now, will add more attributtes.
    - Details view only works the first time it's used.
    - Edit works but doesn't show the results on the list until you refresh.

--- 05-angularjs/app.js
+++ 05-angularjs/app.js
@@ -1,6 +1,13 @@
-var playlistApp = angular.module("playlistApp", ['ui.router']);
+var playlistApp = angular.module("playlistApp", ["ui.router", "LocalStorageModule"]);
 
-playlistApp.config(function ($urlRouterProvider, $stateProvider) {
+playlistApp.config(function (localStorageServiceProvider) {
+  localStorageServiceProvider
+    .setPrefix('playlistApp')
+    .setStorageType('localStorage')
+    .setNotify(true, true)
+});
+
+playlistApp.config(["$urlRouterProvider", "$stateProvider", function ($urlRouterProvider, $stateProvider) {
 	$stateProvider
 	
 	.state("home",{
@@ -9,17 +16,70 @@ playlistApp.config(function ($urlRouterProvider, $stateProvider) {
 		controller:"playlistCtrl"
 	})
 
+	.state("home.details",{
+		url: "/details",
+		templateUrl:"templates/details.html",
+		controller:"detailsCtrl"
+	})
+
+	.state("home.edit",{
+		url: "/edit",
+		templateUrl:"templates/edit.html",
+		controller:"playlistCtrl"
+	})
+
 	$urlRouterProvider.otherwise("/");
 	
-});
+}]);
 
 
-playlistApp.controller("playlistCtrl", ["$scope", function ($scope) {
-	$scope.playlist = ["ASD", "asd"];
+playlistApp.controller("playlistCtrl", ["$scope", "localStorageService", "detailsService", function ($scope, localStorageService, detailsService) {
+	var playlistInStore = localStorageService.get("playlist");
+
+	$scope.playlist = playlistInStore || [];
+
+	$scope.$watch('playlist', function () {
+		localStorageService.set('playlist', $scope.playlist);
+	}, true);
+
 	$scope.addTrack = function (){
 		if ($scope.newtrack){
 			$scope.playlist.push($scope.newtrack);
 			$scope.newtrack = "";
 		}
 	};
+
+	$scope.removeTrack = function (index){
+		$scope.playlist.splice(index, 1);
+	};
+
+	$scope.details = function (trackDetails) {
+		detailsService.setTrackDetails(trackDetails);
+	};
+
+	$scope.edit = function (index) {
+		$scope.editedIndex = index;
+	};
+
+	$scope.saveEdit = function () {
+		$scope.playlist[$scope.editedIndex] = $scope.editedTrack;
+	};
+
 }]);
+
+playlistApp.controller("detailsCtrl", ["$scope", "detailsService", function ($scope, detailsService) {
+	$scope.trackDetails = detailsService.getTrackDetails();
+}]);
+
+playlistApp.service("detailsService", function() {
+	var details = "";
+
+	this.setTrackDetails = function (trackDetails) {
+		details = trackDetails;
+	};
+
+	this.getTrackDetails = function () {
+		return details;
+	};
+	
+});
